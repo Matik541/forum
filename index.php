804ee148ef35d04eb520b995db1bb90608f6dc4c
Matik <mateuszkowaslki199700@gmail.com>
2022-03-15 21:33:22 +0100

    first steps to posts

--- index.php
+++ index.php
@@ -2,12 +2,6 @@
 require_once("./server.php");
 session_start();
 
-try {
-  $con = new PDO("mysql:host=$server;dbname=$basePath", $user, $password);
-  echo "Connecting to $server";
-} catch (PDOException $e) {
-  echo "Error connecting to $server: " . $e->getMessage();
-}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -21,17 +15,71 @@ try {
 </head>
 
 <body>
-  <div id="content">
+  <div class="content">
+    <style>
+      .form {
+        padding: 0;
+      }
 
+      .group{
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        gap: 5px;
+      }
+
+      .form .group .select {
+        width: 75%;
+      }
+
+      .form .group button {
+        width: 25%;
+      }
+    </style>
+    <form method="post" class="form">
+      <input type="text" placeholder="title" name="title" required>
+      <div class="group">
+        <div class="select">
+          <select name="category" id="category" value="0" required>
+            <option disabled selected>category</option>
+            <?php
+            $que = $con->query("SELECT DISTINCT `category` FROM `posts`");
+            while ($rec = $que->fetch()) {
+              echo "<option value='$rec[0]'> - $rec[0]</option>";
+            }
+            ?>
+            <option value="custom">Custom</option>
+          </select>
+          <input type="text" placeholder="add" name="custom" id="custom">
+          <script>
+            let select = document.getElementById("select");
+            let category = document.getElementById("category");
+            let custom = document.getElementById("custom");
+            custom.style.display = "none";
+            category.addEventListener("change", function(e) {
+              if (this.value == "custom") {
+                custom.style.display = "inline-block";
+                custom.required = true;
+                category.style.borderRadius = "5px 5px 0 0";
+              } else {
+                custom.style.display = "none";
+                custom.required = false;
+                category.style.borderRadius = "5px";
+              }
+            })
+          </script>
+        </div>
+        <button type="submit">Post</button>
+      </div>
+    </form>
   </div>
 
-  <?php 
-    if(isset($_SESSION['logged'])){
-      include('./logout.php');
-    }
-    else {
-      echo "<a class='login' href='./login.php'>Zaloguj</a>";
-    }
+  <?php
+  if (isset($_SESSION['logged'])) {
+    include('./logout.php');
+  } else {
+    echo "<a class='login' href='./login.php'><button>Zaloguj</button></a>";
+  }
   ?>
 
 </body>
--- login.php
+++ login.php
@@ -9,49 +9,51 @@ session_start();
 <head>
   <meta charset="UTF-8">
   <title>Login</title>
-  <link rel="stylesheet" href="log-reg.css">
+  <link rel="stylesheet" href="main.css">
 </head>
 
 <body>
-  <div id="container">
-    <div class="login">
-      <div class="form">
-        <form class="login-form" method="post">
-          <input type="text" placeholder="Login" name="login" required />
-          <div id="icon">
-            <input type="password" placeholder="Password" name="password" id="password" required />
-            <a> <span id="visiblity-toggle" class="material-icons-outlined">visibility</span> </a>
-          </div>
-          <button type="submit">login</button>
+  <div class="login">
+    <div class="form">
+      <form class="login-form" method="post">
+        <input type="text" placeholder="Login" name="login" required title="Your email or nickname" />
+        <div id="icon">
+          <input type="password" placeholder="Password" name="password" id="password" required />
+          <a> <span id="visiblity-toggle" class="material-icons-outlined">visibility</span> </a>
+        </div>
+        <button type="submit">login</button>
+        <div id="err">
           <?php
           if (!empty($_POST['login']) && !empty($_POST['password'])) {
-            $que = $con->query("SELECT * FROM `users` WHERE (`email` = '" . $_POST['login'] . "' OR `nick` = '" . $_POST['login'] . "') AND `password` = '" . hash('whirlpool', $_POST['password']) . "';");
+            $que = $con->query("SELECT * FROM `users` WHERE (`email` = '" . $_POST['login'] . "' OR `nick` = '" . $_POST['login'] . "') AND `password` = '" . hash($hash, $_POST['password']) . "';");
             if ($que->fetch()) {
               $_SESSION['logged'] = $_POST['login'];
               header('Location:index.php');
+            } else {
+              echo "<hr>Wrong login or password!";
             }
           }
           ?>
-          <script>
-            let pass = document.querySelector('#password')
-            let btn = document.querySelector('#icon a span')
+        </div>
+        <script>
+          let pass = document.querySelector('#password')
+          let btn = document.querySelector('#icon a span')
 
-            btn.addEventListener('click', () => {
-              if (pass.type === "text") {
-                pass.type = "password";
-                btn.innerHTML = "visibility";
-              } else {
-                pass.type = "text";
-                btn.innerHTML = "visibility_off";
+          btn.addEventListener('click', () => {
+            if (pass.type === "text") {
+              pass.type = "password";
+              btn.innerHTML = "visibility";
+            } else {
+              pass.type = "text";
+              btn.innerHTML = "visibility_off";
 
-              }
-            })
-          </script>
-        </form>
-        <hr>
-        <a href="./register.php">Sign up</a>
-        <!-- <a href="./forgot.php">Forgot password?</a> -->
-      </div>
+            }
+          })
+        </script>
+      </form>
+      <hr>
+      <a href="./register.php">Sign up</a>
+      <!-- <a href="./forgot.php">Forgot password?</a> -->
     </div>
   </div>
 
